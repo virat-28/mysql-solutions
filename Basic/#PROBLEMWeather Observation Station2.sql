15f46b6599c370fd97a0dd59a24212b6437fe5f8
Virat <bachivirat@gmail.com>
2026-07-14 23:37:13 +0530

    Solved Basic sql priblem

---
+++ Basic/#PROBLEMWeather Observation Station2.sql
@@ -0,0 +1,5 @@
+#PROBLEM:Weather Observation Station 3
+#http://hackerrank.com/challenges/weather-observation-station-3/problem?isFullScreen=true
+SELECT DISTINCT CITY
+FROM STATION
+WHERE MOD(ID, 2) = 0;
